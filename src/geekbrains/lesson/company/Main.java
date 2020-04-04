76415abe516ebdf4b213e5975825197f0fce05cc
Ивакин Александр Николаевич <pwian@mail.ru>
2020-04-04 19:04:18 +0300

    add project

---
+++ src/geekbrains/lesson/company/Main.java
@@ -0,0 +1,10 @@
+package geekbrains.lesson.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Я буду изучать Java!");
+        System.out.println("I will learn Java!");
+    }
+}
