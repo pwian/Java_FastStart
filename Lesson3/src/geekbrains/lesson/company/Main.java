a9e4099fd782bf98f01b364d93afe40f6482a9cb
Aleksandr Ivakin <pwian@mail.ru>
2020-04-07 19:15:05 +0300

    lesson 4

--- Lesson3/src/geekbrains/lesson/company/Main.java
+++ Lesson3/src/geekbrains/lesson/company/Main.java
@@ -3,14 +3,19 @@ package geekbrains.lesson.company;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Guess the number");
         int range = 100;
         int number = (int)(Math.random() * range);
-        System.out.println("Print the number from 0 to " + range);
+        playLevel(range, number);
+        scanner.close();
+    }
+
+    private static void playLevel(int range, int number){
         while (true){
+            System.out.println("Print the number from 0 to " + range);
             int input_number = scanner.nextInt();
             if (input_number == number){
                 System.out.println("You are right");
@@ -21,6 +26,6 @@ public class Main {
                 System.out.println("Need More");
             }
         }
-        scanner.close();
+
     }
 }
